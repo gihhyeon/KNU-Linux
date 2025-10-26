e45d7f3311cfcd75243302123f020b9398eec2ff
gihhyeon <fineblue.kh@gmail.com>
2025-10-26 23:10:13 +0900

    테일러 급수 프로그램 병렬 수행 실습 과제

--- lect05/taylor_multiprocess.c
+++ lect05/taylor_multiprocess.c
@@ -1,53 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
+
 #define N 4
-void sinx_taylor(int num_elemnets, int terms, double* x, double* result)
+#define MAXLINE 100
+
+double calculate_sin_taylor(double x, int terms)
+{
+	double value = x;
+    	double numer = x * x * x;
+    	double denom = 6.; // 3!
+    	int sign = -1;
+
+    	for (int j = 1; j <= terms; j++) {
+        	value += (double)sign * numer / denom;
+        	numer *= x * x;
+        	denom *= (2.*(double)j + 2.) * (2.*(double)j + 3.);
+        	sign *= -1;
+    	}	
+    	return value;
+}
+
+void sinx_taylor(int num_elements, int terms, double* x, double* result)
 {
 	int pid;
-	int child_id;
-	int fd[2*n], length;
-	char message[MAXLINE], line[MAXLINE];
+	int status;
+	int fd[2 * num_elements];
+    	char message[MAXLINE], line[MAXLINE];
+    	int length;
 
 	for (int i = 0; i < num_elements; i++) {
-		pipe(fd + 2*i);
+		if (pipe(fd + 2*i) == -1) {
+			perror("pipe");
+		   	exit(1);
+        	}
 
-		child_id = i;
 		pid = fork();
+
+		if (pid == -1) {
+          	 	perror("fork");
+           		exit(1);
+       		}
 
 		if (pid == 0) {
 			close(fd[2*i]);
 
-			double value = x[i];
-               		double numer = x[i] * x[i] * x[i];
-                	double denom = 6.; // 3!
-                	int sign = -1;
+			double value = calculate_sin_taylor(x[i], terms);
 
+			sprintf(message, "%lf", value);
+            		length = strlen(message) + 1;
+            		write(fd[2*i + 1], message, length);
 
-			for(int j=1; j<=terms; j++) {
-				value += (double)sign * numer / denom;
-				numer *= x[i] * x[i];
-				denom *= (2.*(double)j+2.) * (2.*(double)j+3.);
-				sign *= -1;
-		}
-
-		result[i] = value;
-
-		sprintf(message, "%lf", result[i]);
-		length = strlen(message) + 1;
-		write(fd[2*i + 1], message, length);
-
-		exit(child_id);
+			close(fd[2*i + 1]);
+			exit(i);
 		} else {
-			for (int i = 0; i < num_elements; i++) {
-				int status;
-				wait(&status);
-				int child_id = status >> 8;:
-				read(fd[2*child_id], line, MAXLINE);
-				result[child_id] = atof(line);
+			close(fd[2*i + 1]);
 		}
 	}
+
+	for (int i = 0; i < num_elements; i++) {
+		wait(&status);
+
+		int child_id = status >> 8;
+
+		read(fd[2*child_id], line, MAXLINE);
+
+		result[child_id] = atof(line);
+
+		close(fd[2*child_id]);
+	}
 }
+
 int main()
 {
 	double x[N] = {0, M_PI/6., M_PI/3., 0.134};
